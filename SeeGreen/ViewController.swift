99192a8220deba15781a4ff9775f6e209593d601
flin1 <flocean682@gmail.com>
2021-10-18 15:04:54 -0500

    Initial Commit

---
+++ SeeGreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeGreen
+//
+//  Created by student on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
